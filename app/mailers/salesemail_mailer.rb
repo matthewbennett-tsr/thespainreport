4fe61c1b894e124070961c71b962fe1c6f1f74e2
Matthew Bennett <matthew@thespainreport.com>
2015-11-23 12:27:20 +0100

    Adds from to salesemails

--- app/mailers/salesemail_mailer.rb
+++ app/mailers/salesemail_mailer.rb
@@ -1,5 +1,6 @@
 class SalesemailMailer < ApplicationMailer
-
+  default :from => "The Spain Report <subscriptions@thespainreport.com>"
+  
   def send_onboard_email(email, user)
     @salesemail = email
     @user = user
