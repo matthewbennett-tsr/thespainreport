f2f8e531c72fd53979abbef99aba12939a14284f
Matthew Bennett <matthew@thespainreport.com>
2015-12-26 18:14:35 +0100

    Reduce searchlimit to 300

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -12,7 +12,7 @@ class Entry < ActiveRecord::Base
   end
   
   scope :indexlimit, -> {order('updated_at DESC').limit(250)}
-  scope :searchlimit, -> {order('updated_at DESC').limit(500)}
+  scope :searchlimit, -> {order('updated_at DESC').limit(300)}
   scope :world_all, -> {joins(:feed).merge(Feed.world_all)}
   scope :spain_all, -> {joins(:feed).merge(Feed.spain_all)}
   scope :teaser_limit, -> {order('updated_at DESC').limit(10)}
