2d6081ab249701a404388529c05b8206580e6a5c
Matthew Bennett <matthew@thespainreport.com>
2016-03-09 00:02:46 +0100

    Adds HTML e-mails

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,13 +1,16 @@
 class ArticleMailer < ApplicationMailer
+  helper ApplicationHelper
   default :from => "The Spain Report <subscriptions@thespainreport.com>"
 
   def send_article_full new_mail, user
     @article = new_mail
+    @user = user
     mail(:to => "<#{user.email}>", :subject => send_article_subject)
   end
   
   def send_article_teaser new_mail, user
     @article = new_mail
+    @user = user
     mail(:to => "<#{user.email}>", :subject => send_article_subject)
   end
   
@@ -17,7 +20,7 @@ class ArticleMailer < ApplicationMailer
     elsif @article.urgency == 'breaking'
       "BREAKING: #{@article.headline}"
     elsif @article.urgency == 'latest'
-      "LATEST: #{@article.headline}"
+      "LATEST: #{@article.headline}"	
     else
       "#{@article.type.name}: #{@article.headline}"
     end
--- app/mailers/newsitem_mailer.rb
+++ app/mailers/newsitem_mailer.rb
@@ -1,19 +1,24 @@
 class NewsitemMailer < ApplicationMailer
+  helper ApplicationHelper
   default :from => "The Spain Report <subscriptions@thespainreport.com>"
 
   def send_newsitem_full new_mail, user
     @newsitem = new_mail
+    @user = user
     mail(:to => "<#{user.email}>", :subject => send_newsitem_subject)
   end
   
   def send_newsitem_teaser new_mail, user
     @newsitem = new_mail
+    @user = user
     mail(:to => "<#{user.email}>", :subject => send_newsitem_subject)
   end
   
   def send_newsitem_subject
     if @newsitem.article.present? && @newsitem.article.type.name == "LIVE BLOG"
       "LIVE BLOG UPDATE: #{@newsitem.slug}"
+    elsif @newsitem.article.present?
+      "ARTICLE UPDATE: #{@newsitem.slug}"
     else
       "UPDATE: #{@newsitem.slug}"
     end
