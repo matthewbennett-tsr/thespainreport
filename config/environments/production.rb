bd2e43e7668b5c73e37f3bfc2322016fd63797b8
Matthew Bennett <matthew@thespainreport.com>
2016-04-28 15:22:59 +0200

    Trying to fix smtp problem with https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'email-smtp.eu-west-1.amazonaws.com',
+    address: 'https://news.thespainreport.com',
     port: 587,
     domain: 'thespainreport.com',
     user_name: ENV["SMTP_USER"],
