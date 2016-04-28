69d8e63b82a02f6d5b0a0b117f3f81d23bee4051
Matthew Bennett <matthew@thespainreport.com>
2016-04-28 15:37:15 +0200

    Trying amazon ses instead of news again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'https://news.thespainreport.com',
+    address: 'email-smtp.eu-west-1.amazonaws.com',
     port: 587,
     domain: 'thespainreport.com',
     user_name: ENV["SMTP_USER"],
