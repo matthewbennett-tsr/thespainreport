633f3d1c13ab5ab264ce8a8d3dc9f6254ca6121d
Matthew Bennett <matthew@thespainreport.com>
2016-07-25 00:07:34 +0200

    Sorts out created_at dates in article.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,13 @@ class WelcomeController < ApplicationController
 
   def index
     @lasttopstory = Article.topstory.published.lastone
+    @lasttwenty = Article.published.lasttwenty.not_top.not_blog
     @lasteditorial = Article.editorial.published.lastone
     @lastindepth = Article.in_depth.published.lastone
     @lastpolitics = Article.published.politics.lastfive
     @lasteconomy = Article.published.economy.lastfive
     @lastdiplomacy = Article.published.diplomacy.lastfive
-    @lasttwenty = Article.published.lasttwenty.not_top.not_blog
+    
     @latestupdates = Newsitem.published.lastten
     @breakingonly = Article.breakingonly.published.latest.ticker
     @latestaudio = Audio.lastone
--- app/models/article.rb
+++ app/models/article.rb
@@ -25,20 +25,20 @@ class Article < ActiveRecord::Base
   scope :news, -> {where(:type_id => [2,31,32])}
   scope :bignews, -> {where(:urgency => ['latest', 'breaking', 'majorbreaking'])}
   scope :breakingonly, -> {where(:urgency => ['breaking', 'majorbreaking'])}
-  scope :latest, -> {order('updated_at DESC').where(:updated_at => (Time.now - 24.hours)..Time.now)}
+  scope :latest, -> {order('created_at DESC').where(:created_at => (Time.now - 24.hours)..Time.now)}
   scope :ticker, -> {limit(1)}
   scope :politics, -> {joins(:categories).merge(Category.politics)}
   scope :economy, -> {joins(:categories).merge(Category.economy)}
   scope :diplomacy, -> {joins(:categories).merge(Category.diplomacy)}
   scope :published, -> {where(status: ['published', 'updated'])}
-  scope :lastone, -> {order('updated_at DESC').limit(1)}
-  scope :lastfour, -> {order('updated_at DESC').limit(4)}
+  scope :lastone, -> {order('created_at DESC').limit(1)}
+  scope :lastfour, -> {order('created_at DESC').limit(4)}
   scope :lastfive, -> {order('created_at DESC').limit(5)}
   scope :lastsix, -> {order('created_at DESC').limit(6)}
-  scope :lastseven, -> {order('updated_at DESC').limit(7)}
+  scope :lastseven, -> {order('created_at DESC').limit(7)}
   scope :lastten, -> {order('created_at DESC').limit(10)}
   scope :lasttwenty, -> {order('created_at DESC').limit(20)}
-  scope :lastthirty, -> {order('updated_at DESC').limit(30)}
+  scope :lastthirty, -> {order('created_at DESC').limit(30)}
   scope :lastfewdays, -> {where(:created_at => 7.days.ago...1.days.ago).limit(50)}
   scope :last24, -> {where(:created_at => 24.hours.ago..DateTime.now.in_time_zone)}
   scope :next24, -> {where('created_at <= ? and created_at >= now()', 24.hours.from_now).limit(20)}
