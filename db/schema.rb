63d63bfe8f24151ee93cb1ea78c6ea4b26607f63
Matthew Bennett <matthew@thespainreport.com>
2016-11-23 17:21:42 +0100

    Update character count to 1000, adds db column for is_author

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813092321) do
+ActiveRecord::Schema.define(version: 20161106205513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -268,6 +268,7 @@ ActiveRecord::Schema.define(version: 20160813092321) do
     t.string   "emailpref"
     t.string   "stripe_customer_id"
     t.boolean  "allow_access",           default: false
+    t.boolean  "is_author",              default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
