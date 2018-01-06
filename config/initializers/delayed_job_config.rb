3c6e0c9d7e18435dd5a801998bb4fc085bd94361
Matthew Bennett <matthew@thespainreport.com>
2018-01-06 20:42:11 +0100

    Gets rid of another n+1 query for article update/creation, delayed job get rid of failed jobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,6 @@
 # config/initializers/delayed_job_config.rb
 
-Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.destroy_failed_jobs = true
 Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 5
 Delayed::Worker.max_run_time = 5.minutes
