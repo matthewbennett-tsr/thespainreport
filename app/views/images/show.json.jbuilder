bd8bb15ac551f3bf966ddc1701d176289fa68734
Matthew Bennett <matthew@thespainreport.com>
2018-01-07 12:21:06 +0100

    Adds Image class, for multiple images in articles via markdown

---
+++ app/views/images/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @image, :id, :url, :description, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231144729) do
+ActiveRecord::Schema.define(version: 20180107101716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,13 @@ ActiveRecord::Schema.define(version: 20171231144729) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "url"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "invoices", force: :cascade do |t|
