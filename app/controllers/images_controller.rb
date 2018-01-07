05b6116d942129d54992fc83874229fc9020c4ca
Matthew Bennett <matthew@thespainreport.com>
2018-01-07 13:12:02 +0100

    Redirects image create, update to index, adds line break for image captions

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -54,7 +54,7 @@ class ImagesController < ApplicationController
       @image = Image.new(image_params)
       respond_to do |format|
         if @image.save
-          format.html { redirect_to @image, notice: 'Image was successfully created.' }
+          format.html { redirect_to images_url, notice: 'Image was successfully created.' }
           format.json { render :show, status: :created, location: @image }
         else
           format.html { render :new }
@@ -74,7 +74,7 @@ class ImagesController < ApplicationController
     elsif current_user.role == 'editor'
       respond_to do |format|
         if @image.update(image_params)
-          format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+          format.html { redirect_to images_url, notice: 'Image was successfully updated.' }
           format.json { render :show, status: :ok, location: @image }
         else
           format.html { render :edit }
