9adeb96bf756ed8738388c04e88969bc1d663e7f
Matthew Bennett <matthew@thespainreport.com>
2018-01-10 21:18:24 +0100

    Something didn't work, removing Stripe Event config info for now

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,19 +7,3 @@ Rails.configuration.stripe = {
 	secret_spain_key:		ENV['STRIPE_SPAIN_SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
-StripeEvent.signing_secret = whsec_1ufIAmEkRokJEbU3LlkJPg4ZvTYT7hKb
-
-StripeEvent.configure do |events|
-	events.subscribe 'charge.failed' do |event|
-							# Define subscriber behavior based on the event object
-		event.class			#=> Stripe::Event
-		event.type			#=> "charge.failed"
-		event.data.object	#=> #<Stripe::Charge:0x3fcb34c115f8>
-		head 200
-	end
-
-	events.all do |event|
-		# Handle all event types - logging, etc.
-	end
-end
